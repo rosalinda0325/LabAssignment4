94a29cc807a0cd20510dce92cbb114c6455f1a4c
rosalinda0325 <rosalinda03@mail.fresnostate.edu>
2021-12-15 12:38:42 -0800

    finished master

---
+++ src/app/shared-components/shared-components.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SimilarProductCardComponent } from './similar-product-card/similar-product-card.component';
+
+
+
+@NgModule({
+  declarations: [
+    SimilarProductCardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedComponentsModule { }
---
+++ src/app/shared-components/similar-product-card/similar-product-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-similar-product-card',
+  templateUrl: './similar-product-card.component.html',
+  styleUrls: ['./similar-product-card.component.css']
+})
+export class SimilarProductCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
